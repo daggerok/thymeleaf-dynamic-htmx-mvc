630c1d266dd103bff23862166e2cf2a2096f1e9f
Maksim Kostromin <maksim@beachfront.com>
2023-09-14 01:09:50 -0400

    Upgrade spring-boot and liquibase-r2dbc-spring-boot-starter up to 3.1.3 version, other versions upgrade.

--- src/test/kotlin/io/github/daggerok/htmx/ThymeleafDynamicHtmxApplicationTests.kt
+++ src/test/kotlin/io/github/daggerok/htmx/ThymeleafDynamicHtmxApplicationTests.kt
@@ -3,10 +3,11 @@ package io.github.daggerok.htmx
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ThymeleafDynamicHtmxApplicationTests {
 
     @Test
-    fun contextLoads() {
+    fun `should test context`() {
+        // don't know how to test if page hasn't been fully reloaded...
     }
 }
